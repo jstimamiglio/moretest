ea87010684534545c2358e5d37921c5f57ddaf84
jstimamiglio <joaostimamiglio@github.com>
2023-01-24 17:27:54 -0300

    split into components

---
+++ test/src/SearchBar.js
@@ -0,0 +1,15 @@
+
+export default function SearchBar({ filterText, inStockOnly }) {
+ 
+
+    return (
+      <form>
+        <input type="text"  value={filterText}  placeholder="Search..." />
+        <label>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </label>
+      </form>
+    );
+  }
